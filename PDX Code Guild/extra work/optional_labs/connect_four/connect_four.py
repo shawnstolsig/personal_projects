97d91a23d0ead8e6f096730e4447ed18544bd058
Shawn Stolsig <shawnstolsig@gmail.com>
2019-11-14 14:31:59 -0800

    updating file structure after finishing python section

--- PDX Code Guild/extra work/optional_labs/connect_four/connect_four.py
+++
@@ -1,4 +0,0 @@
-# Shawn Stolsig
-# PDX Code Guild
-# Assignment: Optional Lab - Connect Four
-# Date: 11/8/2019
--- PDX Code Guild/lab16-image_manipulation/extra work/reference/clear_cli.py
+++
@@ -1,4 +0,0 @@
-    import os
-    
-    # first, clear terminal window
-    os.system('cls' if os.name == 'nt' else 'clear')
